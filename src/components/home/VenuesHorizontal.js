cc7d6efd9d5daf9a753f835f08fc68f3894d8758
ronakbhalara8-cmyk <ronakbhalara8@gmail.com>
2026-07-27 18:59:54 +0530

    changes
    q

--- src/components/home/VenuesHorizontal.js
+++ src/components/home/VenuesHorizontal.js
@@ -144,7 +144,7 @@ export default function VenuesHorizontal() {
                   className="w-full h-full object-cover"
                 />
                 {/* Soft Gradient Overlay */}
-                <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-black/20 to-transparent" />
+                {/* <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-black/20 to-transparent" /> */}
               </div>
 
               {/* White Overlay Content Card */}
